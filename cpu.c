f2c8cb6f17cc4378786fa699aa838a3981fbd3d1
Simon Johansson <amp@ampleyfly.se>
2017-01-24 21:36:23 +0100

    Add CPU tests

--- cpu.c
+++ cpu.c
@@ -1,10 +1,13 @@
 #include "cpu.h"
 
 void cpu_init(struct cpu_t *cpu, struct prgm_t *prgm, struct state_t *state) {
+    cpu->prgm = prgm;
     cpu->state = state;
-    (void) prgm;
 }
 
 void cpu_step(struct cpu_t *cpu) {
-    cpu->state->pc = 1;
+    if (cpu->prgm->length == 0) {
+        return;
+    }
+    cpu->state->pc = (cpu->state->pc + 1) % cpu->prgm->length;
 }
--- cpu_test.c
+++ cpu_test.c
@@ -1,17 +1,44 @@
 #include "unity.h"
 #include "cpu.h"
 
-void test_Cpu_should_IncPcOnNop(void)
-{
-    struct cpu_t cpu;
-    struct prgm_t prgm = {
-        1,
-        {{OP_NOP, ARG_NONE, ARG_NONE}},
-    };
-    struct state_t state = {
-        0,
-    };
+static struct cpu_t cpu;
+static struct prgm_t prgm;
+static struct state_t state;
+
+void setUp(void) {
+    prgm.length = 0;
+    state.pc = 0;
     cpu_init(&cpu, &prgm, &state);
+}
+
+void test_Cpu_should_NotIncPcOnEmptyProgram(void) {
+    cpu_step(&cpu);
+    TEST_ASSERT_EQUAL_INT(state.pc, 0);
+}
+
+void test_Cpu_should_IncPcOnNop(void) {
+    prgm.length = 2;
+    prgm.instrs[0] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
+    prgm.instrs[1] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
     cpu_step(&cpu);
     TEST_ASSERT_EQUAL_INT(state.pc, 1);
+}
+
+void test_Cpu_should_IncPcTwiceOnTwoNop(void) {
+    prgm.length = 3;
+    prgm.instrs[0] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
+    prgm.instrs[1] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
+    prgm.instrs[2] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
+    cpu_step(&cpu);
+    cpu_step(&cpu);
+    TEST_ASSERT_EQUAL_INT(state.pc, 2);
+}
+
+void test_Cpu_should_WrapAroundPcOnPrgmEnd(void) {
+    prgm.length = 2;
+    prgm.instrs[0] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
+    prgm.instrs[1] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
+    cpu_step(&cpu);
+    cpu_step(&cpu);
+    TEST_ASSERT_EQUAL_INT(state.pc, 0);
 }
