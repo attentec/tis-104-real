ada49182e313c0f51216d546242d2f11b6a66442
Simon Johansson <amp@ampleyfly.se>
2017-02-07 21:41:18 +0100

    WIP: Communication tests

--- cpu_test.c
+++ cpu_test.c
@@ -1,13 +1,37 @@
 #include "unity.h"
 #include "cpu.h"
 
+#define QUEUE_SIZE (100)
+
 #define INSTR0(op)             ((struct instr_t){(op), ARG_NONE, ARG_NONE})
-#define INSTR1(op, arg1)       ((struct instr_t){(op), (arg1), ARG_NONE})
-#define INSTR2(op, arg1, arg2) ((struct instr_t){(op), (arg1), (arg2)})
+#define INSTR1(op, arg1)       ((struct instr_t){(op), (arg1),   ARG_NONE})
+#define INSTR2(op, arg1, arg2) ((struct instr_t){(op), (arg1),   (arg2)})
 
 static struct cpu_t cpu;
 static struct prgm_t prgm;
 static struct state_t state;
+
+static reg_t left_values[QUEUE_SIZE];
+static uint8_t left_read_index;
+static uint8_t left_write_index;
+
+static struct prgm_t test_src_prgm(enum arg_t src) {
+    return (struct prgm_t) {
+        .length = 5,
+        .instrs = {
+            INSTR0(OP_NOP),
+            INSTR1(OP_JRO, src),
+            INSTR0(OP_NOP),
+            INSTR0(OP_NOP),
+            INSTR0(OP_NOP),
+        },
+    };
+}
+
+static void left_sends(reg_t value) {
+    TEST_ASSERT(left_write_index < QUEUE_SIZE);
+    left_values[left_write_index++] = value;
+}
 
 void setUp(void) {
     prgm.length = 0;
@@ -15,6 +39,8 @@ void setUp(void) {
     state.acc = 0;
     state.bak = 0;
     cpu_init(&cpu, &prgm, &state);
+    left_read_index = 0;
+    left_write_index = 0;
 }
 
 void test_Cpu_should_NotIncPcOnEmptyProgram(void) {
@@ -55,16 +81,6 @@ void test_Cpu_should_SetPcOnJump(void) {
     prgm.instrs[1] = INSTR0(OP_NOP);
     cpu_step(&cpu);
     TEST_ASSERT_EQUAL_INT(0, state.pc);
-}
-
-void test_Cpu_should_ReadZeroFromNil(void) {
-    prgm.length = 3;
-    prgm.instrs[0] = INSTR0(OP_NOP);
-    prgm.instrs[1] = INSTR1(OP_JRO, ARG_NIL);
-    prgm.instrs[2] = INSTR0(OP_NOP);
-    state.pc = 1;
-    cpu_step(&cpu);
-    TEST_ASSERT_EQUAL_INT(1, state.pc);
 }
 
 void test_Cpu_should_HaltUsingJro(void) {
@@ -306,4 +322,18 @@ void test_Cpu_should_NegateAccOnNeg(void) {
     state.acc = 999;
     cpu_step(&cpu);
     TEST_ASSERT_EQUAL_INT(-999, state.acc);
+}
+
+void test_Cpu_should_ReadZeroFromNil(void) {
+    prgm = test_src_prgm(ARG_NIL);
+    state.pc = 1;
+    cpu_step(&cpu);
+    TEST_ASSERT_EQUAL_INT(1, state.pc);
+}
+
+void test_Cpu_should_ReadFromLeftOnJroLeft(void) {
+    prgm = test_src_prgm(ARG_LEFT);
+    state.pc = 1;
+    left_sends(2);
+    TEST_ASSERT_EQUAL_INT(3, state.pc);
 }
