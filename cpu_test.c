da803d1bd10aeedf9f3509086b51aae862aa8ace
Simon Johansson <amp@ampleyfly.se>
2017-02-07 19:13:38 +0100

    Refactor instruction constants

--- cpu_test.c
+++ cpu_test.c
@@ -1,6 +1,10 @@
 #include "unity.h"
 #include "cpu.h"
 
+#define INSTR0(op)             ((struct instr_t){(op), ARG_NONE, ARG_NONE})
+#define INSTR1(op, arg1)       ((struct instr_t){(op), (arg1), ARG_NONE})
+#define INSTR2(op, arg1, arg2) ((struct instr_t){(op), (arg1), (arg2)})
+
 static struct cpu_t cpu;
 static struct prgm_t prgm;
 static struct state_t state;
@@ -18,17 +22,17 @@ void test_Cpu_should_NotIncPcOnEmptyProgram(void) {
 
 void test_Cpu_should_IncPcOnNop(void) {
     prgm.length = 2;
-    prgm.instrs[0] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
-    prgm.instrs[1] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
+    prgm.instrs[0] = INSTR0(OP_NOP);
+    prgm.instrs[1] = INSTR0(OP_NOP);
     cpu_step(&cpu);
     TEST_ASSERT_EQUAL_INT(1, state.pc);
 }
 
 void test_Cpu_should_IncPcTwiceOnTwoNop(void) {
     prgm.length = 3;
-    prgm.instrs[0] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
-    prgm.instrs[1] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
-    prgm.instrs[2] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
+    prgm.instrs[0] = INSTR0(OP_NOP);
+    prgm.instrs[1] = INSTR0(OP_NOP);
+    prgm.instrs[2] = INSTR0(OP_NOP);
     cpu_step(&cpu);
     cpu_step(&cpu);
     TEST_ASSERT_EQUAL_INT(2, state.pc);
@@ -36,8 +40,8 @@ void test_Cpu_should_IncPcTwiceOnTwoNop(void) {
 
 void test_Cpu_should_WrapAroundPcOnPrgmEnd(void) {
     prgm.length = 2;
-    prgm.instrs[0] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
-    prgm.instrs[1] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
+    prgm.instrs[0] = INSTR0(OP_NOP);
+    prgm.instrs[1] = INSTR0(OP_NOP);
     cpu_step(&cpu);
     cpu_step(&cpu);
     TEST_ASSERT_EQUAL_INT(0, state.pc);
@@ -45,31 +49,31 @@ void test_Cpu_should_WrapAroundPcOnPrgmEnd(void) {
 
 void test_Cpu_should_SetPcOnJump(void) {
     prgm.length = 2;
-    prgm.instrs[0] = (struct instr_t){OP_JMP, 0, ARG_NONE};
-    prgm.instrs[1] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
+    prgm.instrs[0] = INSTR1(OP_JMP, 0);
+    prgm.instrs[1] = INSTR0(OP_NOP);
     cpu_step(&cpu);
     TEST_ASSERT_EQUAL_INT(0, state.pc);
 }
 
 void test_Cpu_should_ClampPcOnJumpToNegative(void) {
     prgm.length = 2;
-    prgm.instrs[0] = (struct instr_t){OP_JMP, -5, ARG_NONE};
-    prgm.instrs[1] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
+    prgm.instrs[0] = INSTR1(OP_JMP, -5);
+    prgm.instrs[1] = INSTR0(OP_NOP);
     cpu_step(&cpu);
     TEST_ASSERT_EQUAL_INT(0, state.pc);
 }
 
 void test_Cpu_should_ClampPcOnJumpToFarPositive(void) {
     prgm.length = 2;
-    prgm.instrs[0] = (struct instr_t){OP_JMP, 20, ARG_NONE};
-    prgm.instrs[1] = (struct instr_t){OP_NOP, ARG_NONE, ARG_NONE};
+    prgm.instrs[0] = INSTR1(OP_JMP, 20);
+    prgm.instrs[1] = INSTR0(OP_NOP);
     cpu_step(&cpu);
     TEST_ASSERT_EQUAL_INT(prgm.length - 1, state.pc);
 }
 
 void test_Cpu_should_Swap(void) {
     prgm.length = 1;
-    prgm.instrs[0] = (struct instr_t){OP_SWP, ARG_NONE, ARG_NONE};
+    prgm.instrs[0] = INSTR0(OP_SWP);
     state.acc = 100;
     state.bak = 200;
     cpu_step(&cpu);
