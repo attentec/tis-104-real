d0cf94f8795789bdc304fd7ac3e58c2281192cbb
Rasmus Bondesson <rasmus.bondesson@attentec.se>
2019-01-15 09:41:21 +0100

    Remove support for old layout

--- apps/main_avr.c
+++ apps/main_avr.c
@@ -13,8 +13,6 @@
 #define WIDTH (29)
 #define HEIGHT (27)
 
-#define LAYOUT (2)
-
 int main(void) {
     static uint8_t buf[WIDTH * HEIGHT];
     static struct indexmap indices;
@@ -24,16 +22,6 @@ int main(void) {
     static struct spi_t spi;
     static struct tft_t tft;
 
-#if LAYOUT == 1
-    // Display module on prototype shield
-                                                            // Arduino pin
-    struct pin_t led = pin_init(PIN_PORT_D, 2, PIN_DIR_OUTPUT);    // 2
-    struct pin_t clk = pin_init(PIN_PORT_D, 3, PIN_DIR_OUTPUT);    // 3
-    struct pin_t sdi = pin_init(PIN_PORT_D, 4, PIN_DIR_OUTPUT);    // 4
-    struct pin_t rs  = pin_init(PIN_PORT_D, 5, PIN_DIR_OUTPUT);    // 5
-    struct pin_t rst = pin_init(PIN_PORT_D, 6, PIN_DIR_OUTPUT);    // 6
-    struct pin_t cs  = pin_init(PIN_PORT_D, 7, PIN_DIR_OUTPUT);    // 7
-#elif LAYOUT == 2
     // Display module plugged directly into Arduino headers
                                                             // Arduino pin
     struct pin_t led = pin_init(PIN_PORT_C, 0, PIN_DIR_OUTPUT);    // A0
@@ -42,9 +30,6 @@ int main(void) {
     struct pin_t rs  = pin_init(PIN_PORT_C, 3, PIN_DIR_OUTPUT);    // A3
     struct pin_t rst = pin_init(PIN_PORT_C, 4, PIN_DIR_OUTPUT);    // A4
     struct pin_t cs  = pin_init(PIN_PORT_C, 5, PIN_DIR_OUTPUT);    // A5
-#else
-#error Invalid pinout
-#endif
 
     indexmap_init(&indices, WIDTH, HEIGHT, buf);
     screen_init(&scr, &indices);
