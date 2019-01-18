050cf3a44a4866ca0357c692f5d32dcfd74e7ff0
Rasmus Bondesson <rasmus.bondesson@attentec.se>
2019-01-18 17:06:17 +0100

    Remove obsolete indexmap, screen, tft and gui app

--- src/avr/board.c
+++ src/avr/board.c
@@ -1,24 +1,11 @@
 #include "board.h"
 #include "dispif.h"
-#include "font.h"
-#include "fonts.h"
-#include "indexmap.h"
 #include "pin.h"
-#include "screen.h"
 #include "spi.h"
 #include "spi_hw.h"
-#include "tft.h"
 
-#define WIDTH (29)
-#define HEIGHT (27)
-
-static uint8_t buf[WIDTH * HEIGHT];
-static struct indexmap_t indices;
-static struct font_t font;
-static struct screen_t scr;
 static struct dispif_t dispif;
 static struct spi_t spi;
-static struct tft_t tft;
 static struct pin_t led;
 //static struct pin_t clk;
 //static struct pin_t sdi;
@@ -37,14 +24,8 @@ void board_init(struct board_t *board)
     rst = pin_init(PIN_PORT_C, 4, PIN_DIR_OUTPUT);    // A4
     cs  = pin_init(PIN_PORT_C, 5, PIN_DIR_OUTPUT);    // A5
 
-    indexmap_init(&indices, WIDTH, HEIGHT, buf);
-    screen_init(&scr, &indices);
     spi_init(&spi);
     dispif_init(&dispif, &spi, &rs, &cs, &rst, &led);
-    font_init(&font, monoblipp6x8);
-    tft_init(&tft, &dispif, &scr, &font);
 
     board->dispif = &dispif;
-    board->scr = &scr;
-    board->tft = &tft;
 }
--- src/board.h
+++ src/board.h
@@ -1,13 +1,9 @@
 #pragma once
 
-struct screen_t;
 struct dispif_t;
-struct tft_t;
 
 struct board_t {
-    struct screen_t *scr;
     struct dispif_t *dispif;
-    struct tft_t *tft;
 };
 
 void board_init(struct board_t *board);
