dc0b437aee8a2bf15cf2a752b9e5af65b5ef92fb
Rasmus Bondesson <rasmus.bondesson@attentec.se>
2019-04-10 20:58:52 +0200

    Improve delay_* resolution on PC

--- src/pc/delay_pc.c
+++ src/pc/delay_pc.c
@@ -1,12 +1,20 @@
 #include <stdint.h>
-#include <unistd.h>
+#include <time.h>
 
 #include "delay.h"
 
 void delay_ms(uint16_t ms) {
-    (void)sleep(ms / 1000);
+    struct timespec t = {
+        .tv_sec = ms / 1000,
+        .tv_nsec = (ms % 1000L) * 1000000L,
+    };
+    (void)nanosleep(&t, NULL);
 }
 
 void delay_us(uint16_t us) {
-    (void)sleep(us / 1000000);
+    struct timespec t = {
+        .tv_sec = 0,
+        .tv_nsec = us * 1000L,
+    };
+    (void)nanosleep(&t, NULL);
 }
